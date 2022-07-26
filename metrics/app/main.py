a73798b30ded5edace1e501ef0da2712da0dcb3c
aeksei <aeksei94@gmail.com>
2022-07-26 14:03:08 +0300

    add cors middleware

--- metrics/app/main.py
+++ metrics/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from flask_app import app as flask_app
 from app.routers import router
@@ -7,6 +8,15 @@ from app.core.config import settings
 
 
 app = FastAPI(debug=settings.FASTAPI_DEBUG)
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex='http://.*',
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router, prefix="/api/v2")
 
 app.mount("", WSGIMiddleware(flask_app))
