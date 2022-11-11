2253eb49f18dd88c2142cbabdbe45883d11f049b
RitaMargari <mishina.rita22@yandex.ru>
2022-11-11 07:17:35 +0000

    Provisions Context return_json option added

--- metrics/calculations/CityMetricsMethods.py
+++ metrics/calculations/CityMetricsMethods.py
@@ -820,33 +820,6 @@ class CollocationMatrix(BaseMethod):
 
 
 # ######################################### New Provisions #################################################
-class BaseMethod():
-
-    def __init__(self, city_model):
-
-        self.city_model = city_model
-        self.city_crs = city_model.city_crs
-        self.mode = city_model.mode
-
-    def validation(self, method):
-        if self.mode == "user_mode":
-            if not self.city_model.methods.if_method_available(method):
-                bad_layers = self.city_model.methods.get_bad_layers(method)
-                raise ValidationError(f'Layers {", ".join(bad_layers)} do not match specification.')
-
-    @staticmethod
-    def get_territorial_select(area_type, area_id, *args):
-        return tuple(df[df[area_type + "_id"] == area_id] for df in args)
-
-    @staticmethod
-    def get_custom_polygon_select(geojson: dict, set_crs, *args):
-        geojson_crs = geojson["crs"]["properties"]["name"]
-        geojson = gpd.GeoDataFrame.from_features(geojson['features'])
-        geojson = geojson.set_crs(geojson_crs).to_crs(set_crs)
-        custom_polygon = geojson['geometry'][0]
-        return tuple(df[df.within(custom_polygon)] for df in args)
-
-    # TODO: add method for slicing object's dataframe with specifed parameter
 class City_Provisions(BaseMethod): 
 
     def __init__(self, city_model: Any, service_types: list, valuation_type: str, year: int,
