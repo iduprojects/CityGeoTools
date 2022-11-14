dbefb3d1c69ae29067b5092d6971f16396738532
aeksei <aeksei94@gmail.com>
2022-11-14 17:14:13 +0300

    feat: return_jsons со значением по умолчанию

--- metrics/calculations/CityMetricsMethods.py
+++ metrics/calculations/CityMetricsMethods.py
@@ -825,7 +825,7 @@ class City_Provisions(BaseMethod):
     def __init__(self, city_model: Any, service_types: list, valuation_type: str, year: int,
                  user_provisions: Optional[dict[str, list[dict]]], user_changes_buildings: Optional[dict],
                  user_changes_services: Optional[dict], user_selection_zone: Optional[dict], service_impotancy: Optional[list],
-                 return_jsons: bool
+                 return_jsons: bool = False
                  ):
         '''
         >>> City_Provisions(city_model,service_types = "kindergartens", valuation_type = "normative", year = 2022).get_provisons()
