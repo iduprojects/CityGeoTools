5daafdd24647e2b4fdf5cfe5697911287308688a
APogo <annapogostina@gmail.com>
2022-12-07 05:29:55 +0300

    metric's split by method, added annotation

--- metrics/calculations/CityMetricsMethods.py
+++ metrics/calculations/CityMetricsMethods.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.filterwarnings('ignore')
 
-from typing import Any, Optional, Literal
+from typing import Any, Optional
 import geopandas as gpd
 import shapely
 import pandas as pd
@@ -16,6 +16,7 @@ import networkit as nk
 import pandas.core.indexes as pd_index
 import pulp
 from sqlalchemy import create_engine
+from traceback import print_exc
 
 from jsonschema.exceptions import ValidationError
 from .utils import nk_routes_between_two_points
@@ -690,7 +691,7 @@ class AccessibilityIsochrones_v2(BaseMethod):
         
         stops, routes = self.get_routes(graph_gdf, source['isochrone_nodes'], travel_type, weight_type) if routes else ([None], [None])
         
-        return [{"isochrone": json.loads(isochrones.to_json()), "routes": [x], "stops": [y]} for x,y in list(zip(routes, stops))]
+        return {"isochrone": json.loads(isochrones.to_json()), "routes": routes, "stops": stops}
 
     def get_routes(self, graph_gdf, selected_nodes, travel_type, weight_type):
         
@@ -936,6 +937,23 @@ class CollocationMatrix(BaseMethod):
 ################################################ Coverage Zones #################################################
 
 class Coverage_Zones(BaseMethod):
+    """
+    Coverage_Zones provides visual analytics on coverage areas of a chosen type of urban services 
+    via one of given methods: (1) radius or (2) isochrone.
+
+    '''
+    Attributes
+    ------
+    city_model
+            City Information Model
+
+    Methods
+    ------
+    get_radius_zone(service_type, radius)
+            Creates a buffer with a defined radius for each service.
+    get_isochrone_zone(service_type, travel_type, weight_value)
+            Creates an isochrone with defined way of transportation and time to travel (in minutes) for each service. 
+    """
 
     def __init__(self, city_model):
         BaseMethod.__init__(self, city_model)
@@ -944,38 +962,83 @@ class Coverage_Zones(BaseMethod):
         self.services = self.city_model.Services.copy()
         self.walk_speed = 4 * 1000 / 60
 
-    def get_coverage_zone(self, service_type, method: Literal['radius', 'isochrone'], radius=None, travel_type = None, weight_value = None, _routes = False):
+    def get_radius_zone(self, service_type: str, radius: Optional[int]):
+        """
+        Creates a buffer with a defined radius for each service.
 
-        services = self.services[self.services["service_code"] == service_type].reset_index(drop=True)
-        service_types = self.service_types
-
-        if method == 'radius':
-
-            if radius != None:
-                radius = radius
-            else:
-                try:
-                    if service_types[service_types['code'] == service_type].iloc[0]['walking_radius_normative'] != 0:
-                        radius = service_types[service_types['code'] == service_type].iloc[0]['walking_radius_normative']
-                    
-                    elif service_types[service_types['code'] == service_type].iloc[0]['public_transport_time_normative'] != 0:
-                        radius = service_types[service_types['code'] == service_type].iloc[0]['public_transport_time_normative'] * self.walk_speed
-                except:
-                    raise ValueError('radius')
-
-            services['geometry'] = services.geometry.buffer(radius) 
-
-            return json.loads(services.reset_index().to_crs(4326).to_json())
+        Parameters
+        ---------
+        service_type: str
+            The type of services to run the method on.
+        radius: int, optional
+            The radius for the buffer.
+            If radius argument is not defined, it tries to get the value from ServicesTypes's standards.
         
-        elif method == 'isochrone':
-            
-            x_from = services['geometry'].x
-            y_from = services['geometry'].y
-            isochrone = AccessibilityIsochrones_v2(self.city_model).get_isochrone(
-                            travel_type, x_from, y_from, weight_value, weight_type = 'time_min', routes = _routes)
-            return isochrone
+        Returns
+        -------
+        FeatureCollection
+
+        Example
+        -------
+        Get coverage zones for schools with radius of 50 meters.
+            CityMetricsMethods.Coverage_Zones(city_model).get_radius_zone(service_type='schools', radius=50)
+        """
+
+        service_types  = self.service_types
+        services = self.services[self.services['service_code'] == service_type].reset_index(drop=True)
+
+        if radius is not None:
+            radius = radius
+        else:
+            try:
+                if service_types[service_types['code'] == service_type].iloc[0]['walking_radius_normative'] != 0:
+                    radius = service_types[service_types['code'] == service_type].iloc[0]['walking_radius_normative']
+
+                elif service_types[service_types['code'] == service_type].iloc[0]['public_transport_time_normative'] != 0:
+                    radius = service_types[service_types['code'] == service_type].iloc[0]['public_transport_time_normative'] * self.walk_speed
+            except:
+                print_exc()
+                raise ValueError('radius')
         
-        return 
+        services['geometry'] = services['geometry'].buffer(radius)
+
+        return json.loads(services.reset_index().to_crs(4326).to_json())
+
+
+    def get_isochrone_zone(self, service_type: str, travel_type:str, weight_value: int):
+        """
+        Creates an isochrone with defined way of transportation and time to travel (in minutes) for each service.
+        The method calls Accessibility_Isochrones_v2.get_isochrone.
+
+        Parameters
+        ---------
+        service_type: str
+            The type of services to run the method on.
+        travel_type: str
+            From Accessibility_Isochrones_v2. 
+            One of the given ways of transportation: "public_transport", "walk" or "drive".
+        weight_value: int
+            From Accessibility_Isochrones_v2.
+            Minutes to travel.
+        
+        Returns
+        -------
+        List
+
+        Example
+        -------
+        Get coverage zones for dentistries using 10 mins pedestrian-ways isochrone.
+            CityMetricsMethods.Coverage_Zones(city_model).get_isochrone_zone(service_type='dentists', travel_type='walk', weight_value = 10)
+        """
+
+        services = self.services[self.services['service_code'] == service_type].reset_index(drop=True)
+
+        x_from = services['geometry'].x
+        y_from = services['geometry'].y
+        
+        isochrone = AccessibilityIsochrones_v2(self.city_model).get_isochrone(travel_type, x_from, y_from, weight_value, weight_type = 'time_min')
+        
+        return isochrone
 
 
 # ######################################### New Provisions #################################################
