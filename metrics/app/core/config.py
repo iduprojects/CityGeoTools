2300acc5673aaed20773e52e80f6ee08c25aa5e5
oneonwar <37445391+oneonwar@users.noreply.github.com>
2023-03-26 14:18:13 +0600

    Revert "Add cities caching"

--- metrics/app/core/config.py
+++ metrics/app/core/config.py
@@ -4,23 +4,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    """Application global config class, attributes can be modified via environment variables."""
-
     BASE_DIR: str = str(Path(__file__).resolve().parent.parent.parent)
-    """Metrics module base directory (CityGeoTools/metrics)."""
-
-    DATA_DIR: str = str(Path(__file__).resolve().parent.parent.parent.parent) # seems to be unused
-    """Whole project base directory."""
-
-    CITIES_CACHE_DIR = str(Path(__file__).resolve().parent.parent.parent.parent / "cities_cache")
-    """Directory with pickle files of cities data to fasten metrics startup process."""
-
-    UPDATE_CITIES_CACHE: bool = False
-    """Indicates whether cached cities need to be re-downloaded on startup."""
+    DATA_DIR: str = str(Path(__file__).resolve().parent.parent.parent.parent)
 
     FASTAPI_DEBUG: bool = True
-    """FastAPI debug flag."""
 
 
 settings = Settings()
-"""Settings singleton to use across application."""
